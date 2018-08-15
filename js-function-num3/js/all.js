1c6e2d4029e314fe4d3e153f206b0d328a06cc0e
jamieshih <34496443+jamieshih@users.noreply.github.com>
2018-08-15 17:27:53 +0800

    20180815

--- js-function-num3/js/all.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('countId').onclick = function(){
-    var hamPrice = 50;
-    var cokePrice = 20;
-    var hamNum = parseInt(document.getElementById('hamNumId').value)*hamPrice;
-    var cokeNum = parseInt(document.getElementById('cokeNumId').value)*cokePrice;
-    document.getElementById('totalId').textContent = hamNum + cokeNum
-}
-
