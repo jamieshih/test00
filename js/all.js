5380c574239aedf0b551f793a537fe1cbc1d048b
jamieshih <34496443+jamieshih@users.noreply.github.com>
2018-09-20 18:29:53 +0800

    123test

---
+++ js/all.js
@@ -0,0 +1,7 @@
+document.getElementById('countId').onclick = function (){
+
+	var hum = parseInt(document.getElementById('humNumId').value); 
+	var coke = parseInt(document.getElementById('cokeNumId').value);
+
+	console.log(hum + coke); 
+}
